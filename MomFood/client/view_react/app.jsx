9f1cff94487daf315aa252a13e7da515d8538f09
zhangmingming <zhmm1@hotmail.com>
2016-01-06 17:26:49 +0100

    here is my event card v0.1.1

--- MomFood/client/view_react/app.jsx
+++ MomFood/client/view_react/app.jsx
@@ -20,11 +20,11 @@ App.index = React.createClass({
                         <div id="main-banner-title-container">
                             <div id="main-banner-title-wrapper">
                                 <div id="main-banner-title">
-                                    <span>Devant nous s'ouvre le souvenir  Derrière, le passé, l'enfance</span>
+                                    <span>Devant nous s'ouvre le souvenir  Derrière, le passé, l'enfance
+                                    </span>
                                 </div>
-                                <div><h1>hello world mingming</h1></div>
                                 <div id="main-banner-description">
-                        <span>Have you ever missed the flavor of your mom's? Those are memories full with joy and laughters.
+                        <span>Have you ever missed the flavor of your moms? Those are memories full with joy and laughters.
                             Come and wake up memories of the food from your childhood. With the choice of editors, you are going
                             to have a brand new old tongue.
                         </span>
@@ -42,6 +42,34 @@ App.index = React.createClass({
                             </div>
                         </div>
 
+                    </div>
+                    <div id="EventCards" >
+                        <div id="RowCards-container">
+                            <div id="RowCenter">
+                                <div className="EventCard">
+                                <span><a id="EventName" href="mingmingzhang.com"><img className="EventImg" src="makalong.png"  alt="makalong" /></a></span>
+                                </div>
+                                <div className="EventCard">
+                                <span><a id="EventName" href="mingmingzhang.com"><img className="EventImg" src="esgarool.png"  alt="esgarool" /></a></span>
+                                </div> 
+                                <div className="EventCard">
+                                <span><a id="EventName" href="mingmingzhang.com"><img className="EventImg" src="crasong.png"  alt="crasong" /></a></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="RowCards-container">
+                            <div id="RowCenter">
+                                <div className="EventCard">
+                                <span><a id="EventName" href="mingmingzhang.com"><img className="EventImg" src="meat.png"  alt="meat" /></a></span>
+                                </div>
+                                <div className="EventCard">
+                                <span><a id="EventName" href="mingmingzhang.com"><img src="mule.png"  alt="mule" width="100%" height="100%" /></a></span>
+                                </div> 
+                                <div className="EventCard">
+                                <span><a id="EventName" href="mingmingzhang.com"><img className="EventImg" src="beger.png"  alt="beger" /></a></span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
