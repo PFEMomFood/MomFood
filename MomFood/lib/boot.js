37f96d1351ef2233c4d73fdd0be72a977a94916f
Elegode <cedric.yang@ihealthlabs.eu>
2016-01-06 06:29:36 +0100

    fixed header

---
+++ MomFood/lib/boot.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Cedric on 06/01/16.
+ */
+
+
+Meteor.startup(function(){
+    if (!Meteor.isServer){
+        window.onscroll = function(){headerScroll()};
+
+        function headerScroll(){
+            if (document.body.scrollTop>50|| document.documentElement.scrollTop>50){
+                document.getElementById("header").className = "fixed-header";
+                console.log(document.getElementById("header").className)
+            }else{
+                document.getElementById("header").className="";
+            }
+        }
+    }
+})
