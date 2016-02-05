0430642ca5a0f07ecb33cc92c59ae913ad265e05
coucou_miya <bingbing.yan@efrei.net>
2016-02-05 09:37:43 +0100

    fb depolyed

--- MomFood/client/view_react/EventCard.jsx
+++ MomFood/client/view_react/EventCard.jsx
@@ -7,7 +7,7 @@ App.EventCard = React.createClass({
            <div className="card-holder col-md-4 col-sm-6 col-xs-12">
                <div className="card grow blur"></div>
                <div className="card-brief">
-                   <div className="card-brief-item "><h3>{this.props.eventData.start}</h3></div>
+                   <div className="card-brief-item "><h3>{this.props.eventData.host}</h3></div>
                    <div className="card-brief-item item-right "><h3>{this.props.eventData.address.city}</h3></div>
                </div>
            </div>
--- MomFood/server/social-config.js
+++ MomFood/server/social-config.js
@@ -10,7 +10,6 @@ ServiceConfiguration.configurations.upsert(
         }
     }
 );
-
 ServiceConfiguration.configurations.upsert(
     { service: "facebook" },
     {
@@ -21,3 +20,16 @@ ServiceConfiguration.configurations.upsert(
         }
     }
 );
+
+/*for deployed website*/
+/*
+ServiceConfiguration.configurations.upsert(
+    { service: "facebook" },
+    {
+        $set: {
+            loginStyle: "popup",
+            appId: "1533281963636882",
+            secret: "d0ce8fc75efe6c2012f734d6225cd645"
+        }
+    }
+);*/
