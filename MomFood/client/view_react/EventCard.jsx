7f97858fa89bd3c2a66fff3987af8005cf4ad980
coucou_miya <bingbing.yan@efrei.net>
2016-02-06 19:42:24 +0100

    eventCard finished

--- MomFood/client/view_react/EventCard.jsx
+++ MomFood/client/view_react/EventCard.jsx
@@ -2,57 +2,39 @@
  * Created by Cedric on 02/02/16.
  */
 App.EventCard = React.createClass({
-    /*hoverHandler:function(event){
-       /!* console.log( event.target);*!/
-        $(event.currentTarget).find(".hidden-card-description").show();
-       /!* $(this.getDOMNode()).find('.card').siblings(".hidden-card-description").show();*!/
+    getInitialState: function(){
+        return {liked:false};
     },
-    blurHandler:function(event){
-        $(event.currentTarget).find(".hidden-card-description").hide();
-        /!*$(this.getDOMNode()).find('.card').siblings(".hidden-card-description").hide();*!/
+    eventLiked:function(event){
+        if(!this.state.liked){
+            this.state.liked=true;
+            $(event.currentTarget).removeClass('fa-heart-o');
+            $(event.currentTarget).addClass('fa-heart');
+        }
+        else {
+            this.state.liked=false;
+            $(event.currentTarget).removeClass('fa-heart');
+            $(event.currentTarget).addClass('fa-heart-o');
+        }
+
     },
-    render(){
-        console.log((this.props.eventData.start));
-       return(
-           <div className="card-holder col-md-4 col-sm-6 col-xs-12" >
-            <div onMouseEnter={this.hoverHandler} onMouseLeave={this.blurHandler}>
-
-
-                   <div className="card grow blur" ></div>
-                   <div className="hidden-card-description" >
-                           <div> <span>{this.props.eventData.title}</span></div>
-                           <div> <span>{this.props.eventData.price}</span></div>
-                           <div> <span>{this.props.eventData.description}</span></div>
-                   </div>
-            </div>
-
-               <div className="card-brief">
-
-                   <div className="card-brief-item "><span>{this.props.eventData.start.toDateString()}</span></div>
-                   <div className="card-brief-item " style={{"textAlign":"center","width":"auto"}}><i className="fa fa-heart-o"></i></div>
-                   <div className="card-brief-item item-right "><span>{this.props.eventData.address.city}</span></div>
-
-               </div>
-           </div>
-       );
-   }*/
     render()
     {
         return (
-        <div className="col-md-4 col-sm-6 col-xs-12">
-            <div className="view view-first ">
-                <img src="/lameloie.jpg" />
-                <div className="mask">
-                    <h2>{this.props.eventData.title}</h2>
-                    <p>{this.props.eventData.description}</p>
-                    <h2>{this.props.eventData.price}</h2>
-                    <a href="#" class="info">Read More</a>
-                </div>
-            </div>
-            <div className="card-brief">
+        <div className="eventCard col-md-4 col-sm-6 col-xs-12">
+                    <div className="view view-first ">
+                        <a href="#"><img src="/lameloie.jpg"/></a>
+                        <div className="mask">
+                            <h2>{this.props.eventData.title}</h2>
+                            <p>{this.props.eventData.description}</p>
+                            <h4 className='card-foot'>{this.props.eventData.price}€ per person</h4>
 
-                <div className="card-brief-item "><span>{this.props.eventData.start.toDateString()}</span></div>
-                <div className="card-brief-item " style={{"textAlign":"center","width":"auto"}}><i className="fa fa-heart-o"></i></div>
+                        </div>
+                    </div>
+                    <div className="card-brief">
+
+                        <div className="card-brief-item "><span>{this.props.eventData.start.toDateString()}</span></div>
+                        <div className="card-brief-item " style={{"textAlign":"center","width":"auto"}}><i className={this.state.liked?'fa fa-heart':'fa fa-heart-o'} onClick={this.eventLiked}></i></div>
                 <div className="card-brief-item item-right "><span>{this.props.eventData.address.city}</span></div>
 
             </div>
